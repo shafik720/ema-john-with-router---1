ac539e4888ef3ae6a4c969971af2289ffd3730ef
shafik720 <shafikrasel5@gmail.com>
2022-11-01 11:41:22 +0600

    dfgfdg

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Components/Header/Header';
 import Body from './Components/Body/Body';
 import { Route, Routes } from 'react-router';
 import About from './Components/About/About';
+import Error from './Components/Error/Error';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route path="/" element={<Body></Body>}></Route>
         <Route path="/home" element={<Body></Body>}></Route>
         <Route path="/about" element={<About></About>}></Route>
-
+        <Route path="*" element={<Error></Error>}></Route>
       </Routes>      
     </div>
   );
