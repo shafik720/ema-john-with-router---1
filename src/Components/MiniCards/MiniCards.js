8c45a650e67f351a52624da16bca695f093fab1a
shafik720 <shafikrasel5@gmail.com>
2022-11-01 20:27:08 +0600

    xfgfdsg

--- src/Components/MiniCards/MiniCards.js
+++ src/Components/MiniCards/MiniCards.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './MiniCards.css'
 
 const MiniCards = (props) => {
-    let {name, price, img, ratings, seller, shipping} = props.index;
+    let {name, price, img, ratings, seller, shipping, quantity} = props.index;
     return (
         <div className="mini-card-div">
             <div className="mini-card-left">
                 <img src={img} alt=""/>
+                <div className="mini-card-left-details">
+                    <p>Price Per Unit : {price} </p>
+                    <p>Quantity : {quantity} </p>
+                </div>
             </div>
             <div className="mini-card-right"></div>
         </div>
