a8f51a2bfe5b3bd3178a4e9bc6933455c1b5a9fb
shafik720 <shafikrasel5@gmail.com>
2022-11-01 20:08:15 +0600

    xfgfdsg

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
 
 const Orders = () => {
     let [products, setProducts] = useProducts();
 
     let [cart, setCart] = useCart(products);
+    console.log(cart);
     return (
-        <div>
-            <div className="left-oreders">
+        <div className="body-div">
+            <div className="left-orders">
             <h2>Order Page :  </h2>
             <h3>Total Products : {products.length} </h3>
             <h3>Total Added Products : {cart.length} </h3>
             </div>
-            <div className="right-oreders"></div>
+            <div className="body-right">
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
