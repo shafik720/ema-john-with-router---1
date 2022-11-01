a6b0303110278fc44cc455307910659fb00859b0
shafik720 <shafikrasel5@gmail.com>
2022-11-01 20:00:16 +0600

    xfgfdsg

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -8,9 +8,12 @@ const Orders = () => {
     let [cart, setCart] = useCart(products);
     return (
         <div>
+            <div className="left-oreders">
             <h2>Order Page :  </h2>
             <h3>Total Products : {products.length} </h3>
             <h3>Total Added Products : {cart.length} </h3>
+            </div>
+            <div className="right-oreders"></div>
         </div>
     );
 };
