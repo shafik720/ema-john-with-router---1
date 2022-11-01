bc7ce1c80e3ac9ca22ccf704324c1eee2afb563d
shafik720 <shafikrasel5@gmail.com>
2022-11-01 20:17:07 +0600

    xfgfdsg

--- src/Components/MiniCards/MiniCards.js
+++ src/Components/MiniCards/MiniCards.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './MiniCards.css'
 
-const MiniCards = () => {
+const MiniCards = (props) => {
+    let {name, price, img, ratings, seller, shipping} = props.index;
     return (
         <div>
-            
+            <h2>{name}</h2>
         </div>
     );
 };
